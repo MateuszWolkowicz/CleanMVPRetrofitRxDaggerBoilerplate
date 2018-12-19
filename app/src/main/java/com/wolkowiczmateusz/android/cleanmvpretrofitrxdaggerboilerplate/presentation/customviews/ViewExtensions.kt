d36dd18176008d832fba6338d2cb7345e4bfe803
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2018-12-19 13:40:28 +0100

    bug fixed

--- app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/customviews/ViewExtensions.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/customviews/ViewExtensions.kt
@@ -1,8 +1,6 @@
 package com.wolkowiczmateusz.android.cleanmvpretrofitrxdaggerboilerplate.presentation.customviews
 
 import android.view.View
-import android.widget.ImageView
-import com.bumptech.glide.Glide
 
 fun View.visible() {
     visibility = View.VISIBLE
@@ -16,4 +14,3 @@ fun View.gone() {
     visibility = View.GONE
 }
 
-fun ImageView.loadImage(url: String) = Glide.with(this).load(url).into(this)
