117bb6b57af5cb14ed3352bee7feeded69d7f90c
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2018-12-23 20:55:36 +0100

    TestData fixed

--- app/src/test/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/TestData.kt
+++ app/src/test/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/TestData.kt
@@ -7,20 +7,20 @@ object TestData {
 
     const val ERROR_MESSAGE = "error"
     val CORRECT_USER_ENTITY = correctTestUserEntity
-    val CORRECT_USER_RESPONSE = correctTestUserResponse
+    val CORRECT_USER_RESPONSE = correctTestUserResponse()
 
     private val correctTestUserEntity: UserEntity
         get() = UserEntity("1", "token", "Mateusz",
                 "wolkowicz.mateusz@gmail.com", "Mateusz", "Wołkowicz")
 
-    fun correctTestUserResponse(): UserResponse {
-        val userResponse = UserResponse()
-        userResponse.username = "Mateusz"
-        userResponse.firstName = "Mateusz"
-        userResponse.lastName = "Wołkowicz"
-        userResponse.email = "wolkowicz.mateusz@gmail.com"
-        userResponse.objectId = "1"
-        userResponse.sessionToken = "token"
-        return userResponse
+    private fun correctTestUserResponse(): UserResponse {
+        return UserResponse(
+                username = "Mateusz",
+                firstName = "Mateusz",
+                lastName = "Wołkowicz",
+                email = "wolkowicz.mateusz@gmail.com",
+                objectId = "1",
+                sessionToken = "token"
+        )
     }
 }
