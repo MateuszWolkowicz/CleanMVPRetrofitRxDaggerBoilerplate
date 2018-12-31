cfbd211e05bbb217094637926c947b80087bf427
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2018-12-31 01:23:04 +0100

    refactoring based on android lint vol.

--- app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/mapper/SortOrderDataMapper.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/mapper/SortOrderDataMapper.kt
@@ -1,11 +1,6 @@
 package com.wolkowiczmateusz.android.cleanmvpretrofitrxdaggerboilerplate.presentation.mapper
 
-import com.wolkowiczmateusz.android.cleanmvpretrofitrxdaggerboilerplate.domain.model.SortOrder
-import com.wolkowiczmateusz.android.cleanmvpretrofitrxdaggerboilerplate.presentation.model.SortOrderModel
-
 import javax.inject.Inject
 
 internal class SortOrderDataMapper @Inject
-constructor() {
-
-}
+constructor()
