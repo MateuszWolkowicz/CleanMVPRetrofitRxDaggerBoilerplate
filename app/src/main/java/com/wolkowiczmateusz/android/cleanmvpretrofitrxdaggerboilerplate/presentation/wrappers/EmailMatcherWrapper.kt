47450dd9eb491bdf5e46c93c6436d88f5bdb99b8
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2019-01-02 00:38:25 +0100

    code reformat

--- app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/wrappers/EmailMatcherWrapper.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/wrappers/EmailMatcherWrapper.kt
@@ -1,5 +1,7 @@
 package com.wolkowiczmateusz.android.cleanmvpretrofitrxdaggerboilerplate.presentation.wrappers
 
+import android.util.Patterns
+
 class EmailMatcherWrapper {
 
     fun isEmailValid(email: String): Boolean {
