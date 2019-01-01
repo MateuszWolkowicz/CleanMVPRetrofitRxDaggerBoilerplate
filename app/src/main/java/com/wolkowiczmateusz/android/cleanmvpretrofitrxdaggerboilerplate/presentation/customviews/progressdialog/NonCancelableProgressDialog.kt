0581e3f9b60e30b7c2750a7ae228fe6be36944dd
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2019-01-01 23:30:58 +0100

    Suppress for ProgressDialog added

--- app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/customviews/progressdialog/NonCancelableProgressDialog.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/cleanmvpretrofitrxdaggerboilerplate/presentation/customviews/progressdialog/NonCancelableProgressDialog.kt
@@ -1,9 +1,10 @@
+@file:Suppress("DEPRECATION")
+
 package com.wolkowiczmateusz.android.cleanmvpretrofitrxdaggerboilerplate.presentation.customviews.progressdialog
 
 import android.app.ProgressDialog
 import android.content.Context
 
-@Suppress("DEPRECATION")
 class NonCancelableProgressDialog : Dialog {
 
     private lateinit var dialog: ProgressDialog
